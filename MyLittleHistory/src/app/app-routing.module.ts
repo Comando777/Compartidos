512f33f3a59fd1f1f515b4c9a16031fb827cc8fe
Comando777 <44825428+Comando777@users.noreply.github.com>
2020-01-27 09:23:17 -0400

    Ruta a otra pagina

--- MyLittleHistory/src/app/app-routing.module.ts
+++ MyLittleHistory/src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'proyectos',
+    loadChildren: () => import('./proyectos/proyectos.module').then( m => m.ProyectosPageModule)
+  },
 ];
 
 @NgModule({
--- MyLittleHistory/src/app/home/home.page.ts
+++ MyLittleHistory/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {  Router  } from '@angular/router';
+import { Route } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,10 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+  navigateToproyectos(){
+    this.router.navigate(['/proyectos']);
+  }
 
 }
